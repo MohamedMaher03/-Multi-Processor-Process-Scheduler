372b021aca815f7393d08cb726e527280e8d81c1
nohaelboghdady <55716798+nohaelboghdady@users.noreply.github.com>
2023-03-29 07:55:58 +0200

    Created Process Class

--- Process-Scheduler/PROCESS.cpp
+++ Process-Scheduler/PROCESS.cpp
@@ -1,1 +1,97 @@
 #include "PROCESS.h"
+PROCESS::PROCESS(int ArrivalTime, int ID, int CPU_Time,
+	int N, int Request_IO, int Duration_IO)
+{
+	AT = ArrivalTime;
+	PID = ID;
+	CT = CPU_Time;
+	NO_OF_IO = N;
+	IO_R = Request_IO;
+	IO_D = Duration_IO;
+
+}
+
+ void PROCESS::set_RT(int response)
+{
+	 RT = response;
+}
+ 
+ void PROCESS::set_TT(int termination)
+ {
+	 TT = termination;
+ }
+
+ void PROCESS::set_TRT(int termination, int ArrivalTime)
+ {
+	 TRT = termination - ArrivalTime;
+ }
+
+ void PROCESS:: set_WT(int termination, int ArrivalTime, int CPU_Time)
+ {
+	 WT = termination - ArrivalTime - CPU_Time;
+ }
+
+ void PROCESS:: set_IsKilled(bool killed)
+ {
+	 IsKilled = killed;
+ }
+
+ void PROCESS::set_IsParent(bool parent)
+ {
+	 IsParent = parent;
+ }
+
+ int PROCESS::get_PID()
+ {
+	 return PID;
+ }
+
+ int PROCESS::get_AT()
+ {
+	 return AT;
+ }
+
+ int PROCESS::get_RT()
+ {
+	 return RT;
+ }
+
+ int PROCESS::get_CT()
+ {
+	 return CT;
+ }
+
+ int PROCESS::get_TT()
+ {
+	 return TT;
+ }
+
+ int PROCESS::get_TRT()
+ {
+	 return TRT;
+ }
+
+ int PROCESS::get_WT()
+ {
+	 return WT;
+ }
+
+ int PROCESS::get_IO_R()
+ {
+	 return IO_R;
+ }
+
+ int PROCESS:: get_IO_D()
+ {
+	 return IO_D;
+ }
+
+ bool PROCESS::get_IsKilled()
+ {
+	 return IsKilled;
+ }
+
+ bool PROCESS::get_IsParent()
+ {
+	 return IsParent;
+ }
--- Process-Scheduler/PROCESS.h
+++ Process-Scheduler/PROCESS.h
@@ -2,6 +2,40 @@
 class PROCESS
 {
 public:
-	PROCESS(int, int, int, int);
-};
+	int PID;
+	int AT;
+	int RT;
+	int CT;
+	int TT;
+	int TRT;
+	int WT;
+	int IO_R;
+	int IO_D;
+	int NO_OF_IO;
+	bool IsKilled;
+	bool IsParent;
 
+
+	PROCESS(int ArrivalTime, int ID, int CPU_Time,
+		int N, int Request_IO, int Duration_IO);
+
+	void set_RT(int response);
+	void set_TT(int termination);
+	void set_TRT(int termination, int ArrivalTime);
+	void set_WT(int termination, int ArrivalTime, int CPU_Time);
+	void set_IsKilled(bool killed);
+	void set_IsParent(bool Parent);
+	int get_PID();
+	int get_AT();
+	int get_RT();
+	int get_CT();
+	int get_TT();
+	int get_TRT();
+	int get_WT();
+	int get_IO_R();
+	int get_IO_D();
+	bool get_IsKilled();
+	bool get_IsParent();
+
+
+};
