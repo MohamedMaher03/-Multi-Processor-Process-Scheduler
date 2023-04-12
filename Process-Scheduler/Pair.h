141b166008b5a9997e18da90755b895e704be89d
Mohamed Maher <m7mdrefaat550@gmail.com>
2023-04-12 10:22:47 +0200

    solve errors

--- Process-Scheduler/Pair.h
+++ Process-Scheduler/Pair.h
@@ -6,29 +6,15 @@ private:
     int first;
     int second;
 public:
-<<<<<<< HEAD
     Pair(int first=-1, int second=-1)  {
         setfirst(first);
         setsecond(second);
-    }
-    void setfirst(int frst) {
-        first = frst;
-    }
-    void setsecond(int scnd) {
-        second = scnd;
-=======
-    Pair() { first = -1; second = -1; };
-
-    Pair(int f, int s) {
-        setfirst(f);
-        setsecond(s);
     }
     void setfirst(int f) {
         first = f;
     }
     void setsecond(int sec) {
         second = sec;
->>>>>>> 3dd6e1aee5255ab031441055d6a7502712be832a
     }
     int getfirst() {
         return first;
