9ce2f6727d922b180642973e7a751c8c4643fe97
Mohamed Hazem <mohamedhazemwwe@gmail.com>
2023-04-17 18:22:32 +0200

    Works Mostly

--- Process-Scheduler/Source.cpp
+++ Process-Scheduler/Source.cpp
@@ -5,29 +5,8 @@ using namespace std;
 
 int main()
 {
-	/*
-	PROCESS* f = new PROCESS(1, 2, 3, 4);
-	PROCESS* s = new PROCESS(1, 1, 3, 4);
-	LinkedPriorityQueue<PROCESS*>RDY;
-	PROCESS* RUN;
-	RDY.enqueue(f);
-	RDY.enqueue(s);
-	RDY.printContents();
-	RDY.dequeue(s);
-	RUN = s;
-	cout << endl<<RUN->get_CT();
-	/*
-	int n = 5;
-	Pair* io;
-	io = new Pair[n];
-	(io+1)->setfirst(3);
-	io->setsecond(4);
-	cout << io[1].getfirst();
-	cout << io[0].getsecond();
-	*/
 	Scheduler* Maestro = new Scheduler();
 	Maestro->SIMULATE();
 	delete Maestro;
-	return 0;
-	
+	return 0;	
 }
