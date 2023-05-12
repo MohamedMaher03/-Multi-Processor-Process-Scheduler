f669cee04cf90718ef8a446ed56a21231b8df685
Mohamed Maher <m7mdrefaat550@gmail.com>
2023-05-13 00:05:59 +0300

    solve maaaaaaany errors

--- Process-Scheduler/LinkedPriorityQueue.h
+++ Process-Scheduler/LinkedPriorityQueue.h
@@ -1,115 +1,255 @@
-#include <iostream>
+
+/*
+This is a program that implements the queue abstract data type using a linked list.
+The queue is implemented as a chain of linked nodes that has two pointers,
+a frontPtr pointer for the front of the queue and a backPtr pointer for the back of the queue.
+*/
+
+/*
+
+The Node: item of type T and a "next" pointer
+-------------
+| item| next | --->
+-------------
+General Queue case:
+
+frontPtr																backPtr
+\											   						/
+\											  					   /
+------------- 	  ------------- 	  ------------- 	  -------------
+| item| next |--->| item| next |--->  | item| next |--->  | item| next |---> NULL
+------------- 	  ------------- 	  ------------- 	  -------------
+
+Empty Case:
+
+frontptr	 backptr
+\	 /
+\	/
+---- NULL ------
+
+
+Single Node Case:
+frontPtr	 backPtr
+\		/
+\	   /
+-----------
+|item| next| -->NULL
+-----------
+
+*/
+#pragma once
+#ifndef LINKED_PRIORITY_QUEUE_
+#define LINKED_PRIORITY_QUEUE_
+
+#include "Node.h"
+#include "QueueADT.h"
+#include <vector>
+#include"PROCESS.h"
 using namespace std;
+template <typename T>
+class LinkedPriorityQueue 
+{
+private:
+
+	Node<T>* backPtr;
+	Node<T>* frontPtr;
+	int length;
+public:
+	LinkedPriorityQueue();
+	bool isEmpty() const;
+	bool enqueue(const T& data, int priority);
+	bool dequeue(T& frntEntry);
+	bool peek(T& frntEntry)  const;
+	void printContents();
+	int getlength();
+	~LinkedPriorityQueue();
+
+	//copy constructor
+	LinkedPriorityQueue(const LinkedPriorityQueue<T>& LQ);
+};
+#endif
+/////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Function: Queue()
+The constructor of the Queue class.
+
+*/
 
 template <typename T>
-class LinkedPriorityQueue {
-private:
-    struct Node 
-    {
-        T data;
-        int priority;
-        Node* next;
-    };
+LinkedPriorityQueue<T>::LinkedPriorityQueue()
+{
+	backPtr = nullptr;
+	frontPtr = nullptr;
 
-    Node* head;
+}
+/////////////////////////////////////////////////////////////////////////////////////////
 
-public:
-    void printContents();
-    LinkedPriorityQueue() : head(nullptr) {}
+/*
+Function: isEmpty
+Sees whether this queue is empty.
 
-    ~LinkedPriorityQueue() {
-        while (head)
-        {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
+Input: None.
+Output: True if the queue is empty; otherwise false.
+*/
+template <typename T>
+bool LinkedPriorityQueue<T>::isEmpty() const
+{
+	return (frontPtr == nullptr);
+}
 
-    bool isEmpty() const 
-    {
-        return head == nullptr;
-    }
+/////////////////////////////////////////////////////////////////////////////////////////
 
-    void enqueue(T& data, int priority) 
-    {
-        Node* newNode = new Node{data, priority, nullptr};
+/*Function:enqueue
+Adds newEntry at the back of this queue.
 
-        if (isEmpty()) {
-            head = newNode;
-            return;
-        }
+Input: newEntry .
+Output: True if the operation is successful; otherwise false.
+*/
 
-        if (priority > head->priority) {
-            newNode->next = head;
-            head = newNode;
-            return;
-        }
+template <typename T>
+bool LinkedPriorityQueue<T>::enqueue(const T& data, int priority) {
+	Node<T>* newNode = new Node<T>{ data, priority, nullptr};
+	if (isEmpty()) {
+		frontPtr = newNode;
+		return true;
+	}
+	if (priority < frontPtr->getpriority()) {
+			newNode->setNext(frontPtr);
+			frontPtr = newNode;
+			return true;
+	}
 
-        Node* current = head;
+	Node<T>* current = frontPtr;
 
-        while (current->next != nullptr && current->next->priority >= priority) {
-            current = current->next;
-        }
+	while (current->getNext() != nullptr && current->getNext()->getpriority() <= priority) {
+		current=current->getNext();
+	}
 
-        newNode->next = current->next;
-        current->next = newNode;
-    }
+	newNode->setNext(current->getNext());
+	current->setNext(newNode);
 
-    T dequeue(T& frntEntry) {
-        if (isEmpty()) 
-            return nullptr;
+	return true;
+}
 
-        Node* temp = head;
-        frntEntry = temp->data;
 
-        head = head->next;
-        delete temp;
 
-        return frntEntry;
-    }
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    T peek(T& frntEntry) const 
-    {
-        if (isEmpty()) 
-        {
-            return nullptr;
-        }
-        frntEntry = head->data;
-        return frntEntry;
-    }
+  /*Function: dequeue
+  Removes the front of this queue. That is, removes the item that was added
+  earliest.
 
-    int size() const 
-    {
-        int count = 0;
+  Input: None.
+  Output: True if the operation is successful; otherwise false.
+  */
 
-        for (Node* current = head; current != nullptr; current = current->next) {
-            ++count;
-        }
+template <typename T>
+bool LinkedPriorityQueue<T>::dequeue(T& frntEntry)
+{
+	if (isEmpty())
+		return false;
 
-        return count;
-    }
+	Node<T>* nodeToDeletePtr = frontPtr;
+	frntEntry = frontPtr->getItem();
+	frontPtr = frontPtr->getNext();
+	// Queue is not empty; remove front
+	if (nodeToDeletePtr == backPtr)	 // Special case: last node in the queue
+		backPtr = nullptr;
 
-    void print() const {
-        for (Node* current = head; current != nullptr; current = current->next) {
-            std::cout << current->data << " ";
-        }
+	// Free memory reserved for the dequeued node
+	delete nodeToDeletePtr;
+	length--;
+	return true;
 
-        std::cout << std::endl;
-    }
-    
-};
+}
+/////////////////////////////////////////////////////////////////////////////////////////
+/*
+Function: peek
+copies the front of this queue to the passed param. The operation does not modify the queue.
+
+Input: None.
+Output: The front of the queue.
+*/
+template <typename T>
+bool LinkedPriorityQueue<T>::peek(T& frntEntry) const
+{
+	if (isEmpty())
+		return false;
+
+	frntEntry = frontPtr->getItem();
+	return true;
+
+}
+
 template<>
 inline void LinkedPriorityQueue<PROCESS*>::printContents()
 {
 
-    for (Node* current = head; current != nullptr; current = current->next) 
-    {
-        if (!current->next)
-        {
-            cout << current->data->get_PID();
-            return;
-        }
-        cout << current->data->get_PID() << ", ";
-    }
+	Node<PROCESS*>* curr = frontPtr;
+
+	while (curr)
+	{
+		if (!curr->getNext())
+		{
+			cout << curr->getItem()->get_PID();
+			return;
+		}
+		cout << curr->getItem()->get_PID() << ",  ";
+		curr = curr->getNext();
+	}
 }
+
+///////////////////////////////////////////////////////////////////////////////////
+/*
+Function: destructor
+removes all nodes from the queue by dequeuing them
+*/
+template <typename T>
+LinkedPriorityQueue<T>::~LinkedPriorityQueue()
+{
+	T temp;
+
+	//Free (Dequeue) all nodes in the queue
+	while (dequeue(temp));
+}
+/////////////////////////////////////////////////////////////////////////////////////////
+/*
+Function: Copy constructor
+To avoid shallow copy,
+copy constructor is provided
+
+Input: LinkedQueue<T>: The Queue to be copied
+Output: none
+*/
+template<typename T>
+int LinkedPriorityQueue<T>::getlength()
+{
+	return length;
+}
+
+template <typename T>
+LinkedPriorityQueue<T>::LinkedPriorityQueue(const LinkedPriorityQueue<T>& LQ)
+{
+	Node<T>* NodePtr = LQ.frontPtr;
+	if (!NodePtr) //LQ is empty
+	{
+		frontPtr = backPtr = nullptr;
+		return;
+	}
+
+	//insert the first node
+	Node<T>* ptr = new Node<T>(NodePtr->getItem());
+	frontPtr = backPtr = ptr;
+	NodePtr = NodePtr->getNext();
+
+	//insert remaining nodes
+	while (NodePtr)
+	{
+		Node<T>* ptr = new Node<T>(NodePtr->getItem());
+		backPtr->setNext(ptr);
+		backPtr = ptr;
+		NodePtr = NodePtr->getNext();
+	}
+}
+
--- Process-Scheduler/Node.h
+++ Process-Scheduler/Node.h
@@ -6,13 +6,18 @@ class Node
 {
 private :
 	T item; // A data item
+	int priority;
 	Node<T>* next; // Pointer to next node
 public :
 	Node();
 	Node( const T & r_Item);	
+	Node(const T& item, int priority, Node<T>* next)
+		: item(item), priority(priority), next(next) {}
 	Node( const T & r_Item, Node<T>* nextNodePtr);
 	void setItem( const T & r_Item);
 	void setNext(Node<T>* nextNodePtr);
+	void setpriority(int p);
+	int getpriority();
 	T getItem() const ;
 	Node<T>* getNext() const ;
 }; // end Node
@@ -47,7 +52,19 @@ template < typename T>
 void Node<T>::setNext(Node<T>* nextNodePtr)
 {
 	next = nextNodePtr;
-} 
+}
+template<typename T>
+inline void Node<T>::setpriority(int p)
+{
+	priority = p;
+}
+
+template<typename T>
+inline int Node<T>::getpriority()
+{
+	return priority;
+}
+
 
 template < typename T>
 T Node<T>::getItem() const
